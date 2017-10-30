09709f58621c491aad8b8fa331aa933dc658b117
nisa <jinnatunnisa32@gmail.com>
2017-10-30 11:38:55 +0600

    Example of a subclass object being assigned into a superclass object

---
+++ src/RefDemo.java
@@ -0,0 +1,60 @@
+	class Box{
+		
+		double width;
+		double height;
+		double depth;
+	
+		Box(Box ob) {
+		
+			width = ob.width;
+			height = ob.height;
+			depth = ob.depth;
+		}
+		Box(double w, double h, double d)
+		{
+			width = w;
+			height = h;
+			depth = d;
+		}
+		Box()
+		{
+			width = -1;
+			height = -1;
+			depth = -1;
+		}
+		Box(double len) {
+			width = height = depth = len;
+		}
+		double volume() {
+			return width * height * depth;
+		}
+	}
+	class BoxWeight extends Box {
+		
+		double weight;
+		
+		BoxWeight(double w, double h, double d, double m)
+		{
+			width = w;
+			height = h;
+			depth = d;
+			weight = m;
+		}
+	}
+	class RefDemo{
+		public static void main(String args[])
+		{
+			BoxWeight weightbox = new BoxWeight(3, 5, 7, 8.37);
+			Box plainbox = new Box();
+			double vol;
+			vol = weightbox.volume();
+			System.out.println("Volume of weightbox is " + vol);
+			System.out.println("Weight of weightbox is " + weightbox.weight);
+			System.out.println();
+			plainbox = weightbox;
+			vol = plainbox.volume();
+			System.out.println("Volume of plainbox is " + vol);
+		}
+		//The following line will not compile because weight is not a valid field in plainbox
+		//System.out.println("Weight of plainbox is " + plainbox.weight);
+	}
